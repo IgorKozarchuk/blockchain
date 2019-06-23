cc0c89676617c2bc4e8086679b869ee6849a6590
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2019-06-23 16:02:17 +0300

    index parameter removed

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 // Blockchain implementation (with Proof-of-Work)
+// https://www.youtube.com/watch?v=SSo_EIwHSd4
 // https://www.youtube.com/watch?v=zVqczFZr124
 // https://www.youtube.com/watch?v=HneatE69814
 
 const SHA256 = require("crypto-js/sha256.js");
 
 class Block {
-	constructor(index, timestamp, data, prevHash = "") {
-		this.index = index;
+	constructor(timestamp, data, prevHash = "") {
 		this.timestamp = timestamp;
 		this.data = data;
 		this.prevHash = prevHash;
 		this.hash = this.calculateHash();
+		// number which has nothing to do with a block
+		// but is changed while mining block to a given difficulty
+		// (calculating hash with certain amount of 0s)
+		// as we can't change timestamp, data, prevHash
 		this.nonce = 0;
 	}
 	calculateHash() {
-		return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.nonce + this.prevHash).toString();
+		return SHA256(this.timestamp + JSON.stringify(this.data) + this.prevHash + this.nonce).toString();
 	}
 	mineBlock(difficulty) {
-		while (this.hash.substring(0, difficulty) !== new Array(difficulty + 1).join("0")) {
+		// while amount of 0s in hash != to difficulty
+		while (this.hash.substring(0, difficulty) !== new Array(difficulty+1).join("0")) {
 			this.nonce++;
 			this.hash = this.calculateHash();
 		}
@@ -28,13 +33,13 @@ class Block {
 class Blockchain {
 	constructor() {
 		this.chain = [this.createGenesisBlock()];
-		this.difficulty = 4;
+		this.difficulty = 4; // amount of 0s in hash - proof of work
 	}
-	createGenesisBlock() {
+	createGenesisBlock() { // 1st block in the blockchain
 		return new Block(0, "01/01/2017", "Genesis Block", "0");
 	}
 	getLatestBlock() {
-		return this.chain[this.chain.length - 1];
+		return this.chain[this.chain.length-1];
 	}
 	addBlock(newBlock) {
 		newBlock.prevHash = this.getLatestBlock().hash;
@@ -44,7 +49,7 @@ class Blockchain {
 	isChainValid() {
 		for (let i = 1; i < this.chain.length; i++) {
 			const currentBlock = this.chain[i];
-			const prevBlock = this.chain[i - 1];
+			const prevBlock = this.chain[i-1];
 			if (currentBlock.hash !== currentBlock.calculateHash()) {
 				return false;
 			}
@@ -62,10 +67,10 @@ class Blockchain {
 let myCoin = new Blockchain();
 
 console.log("Mining block 1...");
-myCoin.addBlock(new Block(1, "02/01/2017", {amout: 4}));
+myCoin.addBlock(new Block("02/01/2017", {amout: 4}));
 
 console.log("Mining block 2...");
-myCoin.addBlock(new Block(2, "03/01/2017", {amout: 10}));
+myCoin.addBlock(new Block("03/01/2017", {amout: 10}));
 
 // console.log(JSON.stringify(myCoin, "", 2));
 // console.log("Is blockchain valid: " + myCoin.isChainValid());
